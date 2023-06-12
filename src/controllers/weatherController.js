4b57eed68f7d3c0109f937897c940c4c12462d3b
prakharmishra-cyber <mprakhar35@gmail.com>
2023-06-13 01:25:08 +0530

    done

---
+++ src/controllers/weatherController.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+async function getDataFromDatabase() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('src/data/data.json', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
+  });
+}
+
+async function saveDataToDatabase(data) {
+  return new Promise((resolve, reject) => {
+    const jsonData = JSON.stringify(data);
+    fs.writeFile('src/data/data.json', jsonData, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+// Level 4: Post Weather Alerts
+async function saveWeatherAlert(alertDetails) {
+   // TODO: Implement this function
+}
+
+module.exports = {
+  saveWeatherAlert
+};
---
+++ src/routes/weatherRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const weatherController = require('../controllers/weatherController');
+
+
+// Level 4: Post Weather Alerts
+router.post('/alerts', async (req, res) => {
+   // TODO: Implement this function
+});
+
+module.exports = router;
